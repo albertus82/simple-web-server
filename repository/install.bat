d86800ae52d26029138f0c711d5b2404721f28aa
Alberto <albertus82@gmail.com>
2017-05-13 13:13:35 +0200

    added -DcreateChecksum=true

--- repository/install.bat
+++ repository/install.bat
@@ -1,1 +1,1 @@
-mvn install:install-file -DlocalRepositoryPath="./" -Dfile="../../JFaceUtils/target/jface-utils-%1.jar" -Dsources="../../JFaceUtils/target/jface-utils-%1-sources.jar" -DgroupId=it.albertus -DartifactId=jface-utils -Dpackaging=jar -Dversion=%1
+mvn install:install-file -DlocalRepositoryPath="./" -Dfile="../../JFaceUtils/target/jface-utils-%1.jar" -Dsources="../../JFaceUtils/target/jface-utils-%1-sources.jar" -DgroupId=it.albertus -DartifactId=jface-utils -Dpackaging=jar -DcreateChecksum=true -Dversion=%1
