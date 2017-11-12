fb0b782f3e717b165ffd5773b214f2d54d4c51c3
Alberto <albertus82@gmail.com>
2017-11-12 20:08:35 +0100

    Refactoring.

--- src/main/java/it/albertus/net/httpserver/SimpleWebServer.java
+++ src/main/java/it/albertus/net/httpserver/SimpleWebServer.java
@@ -1,10 +1,6 @@
 package it.albertus.net.httpserver;
 
-import it.albertus.net.httpserver.AbstractHttpHandler;
-import it.albertus.net.httpserver.FilesHandler;
-import it.albertus.net.httpserver.LightweightHttpServer;
 import it.albertus.net.httpserver.config.HttpServerDefaultConfig;
-import it.albertus.net.httpserver.config.IHttpServerConfig;
 
 public class SimpleWebServer {
 
@@ -13,19 +9,17 @@ public class SimpleWebServer {
 	}
 
 	public static void main(final String... args) {
-		final IHttpServerConfig configuration = new HttpServerDefaultConfig() {
+		final LightweightHttpServer server = new LightweightHttpServer(new HttpServerDefaultConfig() {
 			@Override
-			public AbstractHttpHandler[] getHandlers() {
-				return new AbstractHttpHandler[] { new FilesHandler(this, args[1], "/") };
+			public HttpPathHandler[] getHandlers() {
+				return new HttpPathHandler[] { new FilesHandler(this, args[1], "/") };
 			}
 
 			@Override
 			public int getPort() {
 				return Integer.parseInt(args[0]);
 			}
-		};
-
-		final LightweightHttpServer server = new LightweightHttpServer(configuration);
+		});
 		Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
 		server.start(false);
 	}
